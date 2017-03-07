51ad80ae3af7b276224b55202756197dd10c4bd7
tangerwei <tangerwei@foxmail.com>
2017-03-07 14:55:36 +0800

    init

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ this is a project to using webpack
 <p>
     webpack 基本的打包功能功能
 </p>
+<p>
+优化加载
+<ul>
+    <li>提取公共代码</li>
+    <li>混淆压缩代码</li>
+</ul>
+</p>
+<p>
+    webpack的文档地址
+    <ul>
+        <li><a href="https://webpack.js.org/">webpack首页</a></li>
+    </ul>
+</p>
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
 var webpack = require('webpack');
 var path = require('path');
 var commonPlugin = new webpack.optimize.CommonsChunkPlugin("common");
+var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
+  output: {
+    comments: false,  // remove all comments
+  },
+  compress: {
+    warnings: false
+  }
+})
 var config = {
-  plugins:[commonPlugin],
+  plugins: [commonPlugin,uglifyPlugin],
   entry: {
-    app: path.resolve(__dirname,'lib/app.js'),
-    main: path.resolve(__dirname,'lib/main.js'),
-    index: path.resolve(__dirname,'lib/index.js'),
+    app: path.resolve(__dirname, 'lib/app.js'),
+    main: path.resolve(__dirname, 'lib/main.js'),
+    index: path.resolve(__dirname, 'lib/index.js'),
   },
-  //其他配置
-  output:{
-    filename:'[name].js',
-    path: path.resolve(__dirname,'build'),
+  //其他配置//[name].[hash].js
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'build'),
   },
-  resolve:{
-    extensions:['.js','.jsx']
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias:{
+      'react':path.join(__dirname,'node_modules/react/react.js'),
+      'react-dom':path.join(__dirname,'node_modules/react-dom/dist/react-dom.js')
+    }
   },
-  module:{
-    loaders:[{
-      test:/\.js$/,
-      exclude:/node_modules/,
-      loader:'babel-loader'
-    },{
-      test:/\.scss$/,
-      loader:'style!css!sass'
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
     }]
   }
 
